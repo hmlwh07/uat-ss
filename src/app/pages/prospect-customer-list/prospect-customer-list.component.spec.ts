8c64505a0123f0b2d1bfa56c57e9ad2fbc6b78a5
yuzana <yuzana@blue-stone.net>
2022-03-29 11:29:25 +0630

    29-3-2022

--- src/app/pages/prospect-customer-list/prospect-customer-list.component.spec.ts
+++ src/app/pages/prospect-customer-list/prospect-customer-list.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProspectCustomerListComponent } from './prospect-customer-list.component';
 
 describe('ProspectCustomerListComponent', () => {
