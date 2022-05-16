473652f38548244b5e6bba163cd4f299ee387e59
BoBo <bobo@blue-stone.net>
2022-05-16 17:14:26 +0630

    mod report

--- src/app/pages/lead-detail/FNA-list.const.ts
+++ src/app/pages/lead-detail/FNA-list.const.ts
@@ -1,7 +1,7 @@
 import { COLTYPE, TableCol } from '../../_metronic/shared/crud-table/components/material-table-view/table-dto'
 export const FNACol: TableCol[] = [
   {
-    title: "FNA Id",
+    title: "FNA ID",
     type: COLTYPE.FEILD,
     field: "id"
   },
