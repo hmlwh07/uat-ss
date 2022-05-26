73f216c0b900ab3b990868e1bdf0525ca625188d
May Thu Kha <maythu@blue-stone.net>
2022-05-26 11:24:17 +0630

    added Renewal Search Box (comment closed)

--- src/app/pages/renew-management-list/renew-management-list.component.ts
+++ src/app/pages/renew-management-list/renew-management-list.component.ts
@@ -36,9 +36,8 @@ export class RenewManagementListComponent implements OnInit {
 
   loadForm() {
     this.actForm = new FormGroup({
-      "type": new FormControl(""),
-      "title": new FormControl(null),
-      "status": new FormControl(""),
+      "policyNumber": new FormControl(null),
+      "policyHolder": new FormControl(null),
       startDate: new FormControl(null),
       endDate: new FormControl(null)
     })
@@ -50,6 +49,7 @@ export class RenewManagementListComponent implements OnInit {
       if (res) {
 
         this.renewList = res
+        console.log("RenewList", this.renewList)
         this.cdf.detectChanges()
         this.matTable.reChangeData()
       }
@@ -116,5 +116,16 @@ export class RenewManagementListComponent implements OnInit {
           }
         });
     }
+  }
+
+  getRenewalList() {
+    this.renewService.getRenewList(this.actForm.getRawValue()).toPromise().then((res: any) => {
+      if (res) {
+        this.renewList = res
+        this.cdf.detectChanges()
+        if (this.matTable)
+          this.matTable.reChangeData()
+      }
+    })
   }
 }
