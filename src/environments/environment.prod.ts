7ae2979765f012d30aa4ba7e63e36c4e6c2bca55
Khant Ti Kyi <khant@blue-stone.net>
2022-06-14 13:21:25 +0630

    [Fixed]Issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   appVersion: 'v717demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'https://producersalesuat.kbzms.com/sale/api/v1',
+  apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
   // apiUrl: 'http://104.248.152.205:8082/sale/api/v1',
   // apiUrl: 'http://104.248.152.205:8082/sale/api/v1',
   // apiUrl: 'http://178.128.30.201:8010/sale/api/v1',
