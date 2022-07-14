32cdd4112a7883f0eeae6b1dc3b8cdb7eec0cee7
Khant Ti Kyi <khant@blue-stone.net>
2022-07-14 16:07:37 +0630

    [Prepare]Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,32 +3,32 @@ export const environment = {
   appVersion: 'v717demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  // apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
+  apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
   // apiUrl: 'https://producersalesuat.kbzms.com/sale/api/v1', 
-  apiUrl:'https://producersalespreprod.kbzms.com/sale/api/v1',
+  // apiUrl:'https://producersalespreprod.kbzms.com/sale/api/v1',
   // apiUrl: 'http://104.248.152.205:8082/sale/api/v1',
   // apiUrl: 'http://104.248.152.205:8082/sale/api/v1',
   // apiUrl: 'http://178.128.30.201:8010/sale/api/v1',
   // apiUrl: 'mysite.com/api'
   crm: 'http://178.128.30.201:8089/BlueStoneCrmWeb/api/v1',
-  // firebaseConfig: {
-  //   apiKey: "AIzaSyA83H8v2hiBokOT3lk_YfktW20GRPp-UqM",
-  //   authDomain: "digitalfe-uat.firebaseapp.com",
-  //   projectId: "digitalfe-uat",
-  //   storageBucket: "digitalfe-uat.appspot.com",
-  //   messagingSenderId: "210382068330",
-  //   appId: "1:210382068330:web:930c7c1ac9570df92bc813",
-  //   measurementId: "G-E16237EJL2",
-  //   serverKey: "AAAAMPu_mmo:APA91bEaAJFOiw2fiyMjR2bSVq4102vApYMwtupFltwO-6n-R4EawL92q27OQyWHl9eSOpNjfb0q-BaZA6V1xWAMGojYfzCtRB-ZfxIHSbYZinMA3HV2fNgqWXtAKoZkV5cuJe0KhQdB"
-  // }
-  firebaseConfig : {
-    apiKey: "AIzaSyCCfS97dyHL4eg1kTRMccxpEPzc3EIjQBE",
-    authDomain: "kbzms-preprod.firebaseapp.com",
-    projectId: "kbzms-preprod",
-    storageBucket: "kbzms-preprod.appspot.com",
-    messagingSenderId: "905070136071",
-    appId: "1:905070136071:web:008fb57112495ea676d3a2",
-    measurementId: "G-BCJWF0DB5W",
-    serverKey:"AAAA0rpiSwc:APA91bFsb-tjVv3tzEIn0pHkYtTe1vHknAV979MoIczFYymm1qYBmX32p-AWvuS03ZgRSfF4h0MBbQlDzTnxEitGR7o5vpmUMmZIe-qhCZVbToNaBsq7VWTjxB0JqEKLFKeDikgSfnVo"
+  firebaseConfig: {
+    apiKey: "AIzaSyA83H8v2hiBokOT3lk_YfktW20GRPp-UqM",
+    authDomain: "digitalfe-uat.firebaseapp.com",
+    projectId: "digitalfe-uat",
+    storageBucket: "digitalfe-uat.appspot.com",
+    messagingSenderId: "210382068330",
+    appId: "1:210382068330:web:930c7c1ac9570df92bc813",
+    measurementId: "G-E16237EJL2",
+    serverKey: "AAAAMPu_mmo:APA91bEaAJFOiw2fiyMjR2bSVq4102vApYMwtupFltwO-6n-R4EawL92q27OQyWHl9eSOpNjfb0q-BaZA6V1xWAMGojYfzCtRB-ZfxIHSbYZinMA3HV2fNgqWXtAKoZkV5cuJe0KhQdB"
   }
+  // firebaseConfig : {
+  //   apiKey: "AIzaSyCCfS97dyHL4eg1kTRMccxpEPzc3EIjQBE",
+  //   authDomain: "kbzms-preprod.firebaseapp.com",
+  //   projectId: "kbzms-preprod",
+  //   storageBucket: "kbzms-preprod.appspot.com",
+  //   messagingSenderId: "905070136071",
+  //   appId: "1:905070136071:web:008fb57112495ea676d3a2",
+  //   measurementId: "G-BCJWF0DB5W",
+  //   serverKey:"AAAA0rpiSwc:APA91bFsb-tjVv3tzEIn0pHkYtTe1vHknAV979MoIczFYymm1qYBmX32p-AWvuS03ZgRSfF4h0MBbQlDzTnxEitGR7o5vpmUMmZIe-qhCZVbToNaBsq7VWTjxB0JqEKLFKeDikgSfnVo"
+  // }
 };
