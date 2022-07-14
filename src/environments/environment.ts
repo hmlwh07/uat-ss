c0c785f17a3ad17915f9b0e97af1e28e2e4e5519
Khant Ti Kyi <khant@blue-stone.net>
2022-07-14 10:09:52 +0630

    [Pre]Prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v717demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-   apiUrl: 'http://54.251.196.83:8083/sale/api/v1',
+   apiUrl: 'http://54.251.196.83:8083/sale/api/v1', 
   // apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
   //  apiUrl: 'https://producersalesuat.kbzms.com/sale/api/v1',
   firebaseConfig: {
