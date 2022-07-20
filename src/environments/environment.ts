8ff72e5b7788097ce7ca27f441db3725c9d81eab
Khant Ti Kyi <khant@blue-stone.net>
2022-07-20 13:04:18 +0630

    [Develop]BranchCode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,33 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 //Testing
-export const environment = {
-  production: false,
-  appVersion: 'v717demo1',
-  USERDATA_KEY: 'authf649fc9a5f55',
-  isMockEnabled: false,
-  apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
-  firebaseConfig: {
-    apiKey: "AIzaSyA83H8v2hiBokOT3lk_YfktW20GRPp-UqM",
-    authDomain: "digitalfe-uat.firebaseapp.com",
-    projectId: "digitalfe-uat",
-    storageBucket: "digitalfe-uat.appspot.com",
-    messagingSenderId: "210382068330",
-    appId: "1:210382068330:web:930c7c1ac9570df92bc813",
-    measurementId: "G-E16237EJL2",
-    serverKey: "AAAAMPu_mmo:APA91bEaAJFOiw2fiyMjR2bSVq4102vApYMwtupFltwO-6n-R4EawL92q27OQyWHl9eSOpNjfb0q-BaZA6V1xWAMGojYfzCtRB-ZfxIHSbYZinMA3HV2fNgqWXtAKoZkV5cuJe0KhQdB"
-  }
-};
-
-//UAT
 // export const environment = {
 //   production: false,
 //   appVersion: 'v717demo1',
 //   USERDATA_KEY: 'authf649fc9a5f55',
 //   isMockEnabled: false,
-//   apiUrl: 'http://54.251.196.83:8083/sale/api/v1',
-//   // apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
-//   //  apiUrl: 'https://producersalesuat.kbzms.com/sale/api/v1',
+//   apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
 //   firebaseConfig: {
 //     apiKey: "AIzaSyA83H8v2hiBokOT3lk_YfktW20GRPp-UqM",
 //     authDomain: "digitalfe-uat.firebaseapp.com",
@@ -40,8 +19,29 @@ export const environment = {
 //     measurementId: "G-E16237EJL2",
 //     serverKey: "AAAAMPu_mmo:APA91bEaAJFOiw2fiyMjR2bSVq4102vApYMwtupFltwO-6n-R4EawL92q27OQyWHl9eSOpNjfb0q-BaZA6V1xWAMGojYfzCtRB-ZfxIHSbYZinMA3HV2fNgqWXtAKoZkV5cuJe0KhQdB"
 //   }
-
 // };
+
+//UAT
+export const environment = {
+  production: false,
+  appVersion: 'v717demo1',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: false,
+  apiUrl: 'http://54.251.196.83:8083/sale/api/v1',
+  // apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
+  //  apiUrl: 'https://producersalesuat.kbzms.com/sale/api/v1',
+  firebaseConfig: {
+    apiKey: "AIzaSyA83H8v2hiBokOT3lk_YfktW20GRPp-UqM",
+    authDomain: "digitalfe-uat.firebaseapp.com",
+    projectId: "digitalfe-uat",
+    storageBucket: "digitalfe-uat.appspot.com",
+    messagingSenderId: "210382068330",
+    appId: "1:210382068330:web:930c7c1ac9570df92bc813",
+    measurementId: "G-E16237EJL2",
+    serverKey: "AAAAMPu_mmo:APA91bEaAJFOiw2fiyMjR2bSVq4102vApYMwtupFltwO-6n-R4EawL92q27OQyWHl9eSOpNjfb0q-BaZA6V1xWAMGojYfzCtRB-ZfxIHSbYZinMA3HV2fNgqWXtAKoZkV5cuJe0KhQdB"
+  }
+
+};
 
 //Pre-Production
 // export const environment = {
