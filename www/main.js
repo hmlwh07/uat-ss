237aba0bb0b30dddadd81d700fbddb097e07e46c
Khant Ti Kyi <khant@blue-stone.net>
2022-08-02 09:55:30 +0630

    [Add]APP_ID

--- www/main.js
+++ www/main.js
@@ -2114,7 +2114,7 @@ let AppComponent = class AppComponent {
             formObjs: [],
             configPage: []
         };
-        // this.requestPermission()
+        this.requestPermission();
         const itemsData = localStorage.getItem("itemsData");
         this.itemService.loadItems(JSON.parse(itemsData) || productData);
         const routerSubscription = this.router.events.subscribe((event) => {
@@ -2168,7 +2168,7 @@ let AppComponent = class AppComponent {
                 }
             }
             else {
-                // this.requestPermission()
+                this.requestPermission();
             }
         });
         console.log("PLat", this.plat);
@@ -5979,32 +5979,12 @@ __webpack_require__.r(__webpack_exports__);
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 //Testing
-const environment = {
-    production: false,
-    appVersion: 'v717demo1',
-    USERDATA_KEY: 'authf649fc9a5f55',
-    isMockEnabled: false,
-    apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
-    firebaseConfig: {
-        apiKey: "AIzaSyA83H8v2hiBokOT3lk_YfktW20GRPp-UqM",
-        authDomain: "digitalfe-uat.firebaseapp.com",
-        projectId: "digitalfe-uat",
-        storageBucket: "digitalfe-uat.appspot.com",
-        messagingSenderId: "210382068330",
-        appId: "1:210382068330:web:930c7c1ac9570df92bc813",
-        measurementId: "G-E16237EJL2",
-        serverKey: "AAAAMPu_mmo:APA91bEaAJFOiw2fiyMjR2bSVq4102vApYMwtupFltwO-6n-R4EawL92q27OQyWHl9eSOpNjfb0q-BaZA6V1xWAMGojYfzCtRB-ZfxIHSbYZinMA3HV2fNgqWXtAKoZkV5cuJe0KhQdB"
-    }
-};
-//UAT
 // export const environment = {
 //   production: false,
 //   appVersion: 'v717demo1',
 //   USERDATA_KEY: 'authf649fc9a5f55',
 //   isMockEnabled: false,
-//   apiUrl: 'http://54.251.196.83:8083/sale/api/v1',
-//   // apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
-//   //  apiUrl: 'https://producersalesuat.kbzms.com/sale/api/v1',
+//   apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
 //   firebaseConfig: {
 //     apiKey: "AIzaSyA83H8v2hiBokOT3lk_YfktW20GRPp-UqM",
 //     authDomain: "digitalfe-uat.firebaseapp.com",
@@ -6016,6 +5996,26 @@ const environment = {
 //     serverKey: "AAAAMPu_mmo:APA91bEaAJFOiw2fiyMjR2bSVq4102vApYMwtupFltwO-6n-R4EawL92q27OQyWHl9eSOpNjfb0q-BaZA6V1xWAMGojYfzCtRB-ZfxIHSbYZinMA3HV2fNgqWXtAKoZkV5cuJe0KhQdB"
 //   }
 // };
+//UAT
+const environment = {
+    production: false,
+    appVersion: 'v717demo1',
+    USERDATA_KEY: 'authf649fc9a5f55',
+    isMockEnabled: false,
+    apiUrl: 'http://54.251.196.83:8083/sale/api/v1',
+    // apiUrl: 'http://104.248.152.205:8083/sale/api/v1',
+    //  apiUrl: 'https://producersalesuat.kbzms.com/sale/api/v1',
+    firebaseConfig: {
+        apiKey: "AIzaSyA83H8v2hiBokOT3lk_YfktW20GRPp-UqM",
+        authDomain: "digitalfe-uat.firebaseapp.com",
+        projectId: "digitalfe-uat",
+        storageBucket: "digitalfe-uat.appspot.com",
+        messagingSenderId: "210382068330",
+        appId: "1:210382068330:web:930c7c1ac9570df92bc813",
+        measurementId: "G-E16237EJL2",
+        serverKey: "AAAAMPu_mmo:APA91bEaAJFOiw2fiyMjR2bSVq4102vApYMwtupFltwO-6n-R4EawL92q27OQyWHl9eSOpNjfb0q-BaZA6V1xWAMGojYfzCtRB-ZfxIHSbYZinMA3HV2fNgqWXtAKoZkV5cuJe0KhQdB"
+    }
+};
 //Pre-Production
 // export const environment = {
 //   production: false,
