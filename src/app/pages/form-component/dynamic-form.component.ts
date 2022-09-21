f9044c3027022503526e19a1f511fb36988dbb8f
Khant Ti Kyi <khant@blue-stone.net>
2022-09-21 17:16:00 +0630

    [Fixed]Issues

--- src/app/pages/form-component/dynamic-form.component.ts
+++ src/app/pages/form-component/dynamic-form.component.ts
@@ -118,20 +118,20 @@ export class DynamicFormComponent implements OnInit {
   handleSubmit() {
     console.log(this.form);
     if (this.form.controls['m_policy_term'] ? this.form.controls['m_policy_term'].value == null : false) {
-      this.alert.activate('Please Select Policy Term First', 'Error')
+      this.alert.activate('Please Select Policy Term First.', 'Error')
     } else {
       if (this.form.invalid) {
         if (this.form.controls['m_period_of_insurance_from']?this.form.controls['m_period_of_insurance_from'].errors:false) {
-          this.alert.activate('No back date is allowed. Please select the correct date', 'Error')
+          this.alert.activate('No back date is allowed. Please select the correct date.', 'Error')
         }
         else if (this.form.controls['policy_inception_date']?this.form.controls['policy_inception_date'].errors:false) {
-          this.alert.activate('No back date is allowed. Please select the correct date', 'Error')
+          this.alert.activate('No back date is allowed. Please select the correct date.', 'Error')
         }
         else if (this.form.controls['travel_start_date']?this.form.controls['travel_start_date'].errors:false) {
-          this.alert.activate('No back date is allowed. Please select the correct date', 'Error')
+          this.alert.activate('No back date is allowed. Please select the correct date.', 'Error')
         }
         else if (this.form.controls['formdate']?this.form.controls['formdate'].errors:false) {
-          this.alert.activate('No back date is allowed. Please select the correct date', 'Error')
+          this.alert.activate('No back date is allowed. Please select the correct date.', 'Error')
         }
         else {
           return false
