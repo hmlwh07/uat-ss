47f7f0ff4f33978881cad173730819f6bf087222
Khant Ti Kyi <khant@blue-stone.net>
2023-03-10 12:51:20 +0630

    fix-motor

--- src/app/pages/resourse-detail/resourse-detail.component.ts
+++ src/app/pages/resourse-detail/resourse-detail.component.ts
@@ -18,7 +18,6 @@ import { PrintPreviewModalMobileComponent } from '../products/print-preview-moda
 import { PrintPreviewModalComponent } from '../products/print-preview-modal/print-preview-modal.component';
 import { ProductDataService } from '../products/services/products-data.service';
 import { SignaturePadComponent } from './signature-pad/signature-pad.component';
-
 @Component({
   selector: 'app-resourse-detail',
   templateUrl: './resourse-detail.component.html',
@@ -59,6 +58,7 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
   selectedSourceOfBusiness: string = null
   statusCode
   emailInfo: any
+  quoResult: boolean = true
   constructor(
     private productService: ProductDataService,
     private location: Location,
@@ -74,9 +74,7 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
     private masterDataService: MasterDataService,
     private leadDetailService: LeadDetailService,
   ) { }
-
   async ngOnInit() {
-
     if (!this.productService.createingProd || !this.productService.createingProd.id) {
       this.location.back()
     } else {
@@ -88,21 +86,17 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       this.branch = this.resourceDetail.branchCode
       this.sourceOfBusiness = this.resourceDetail.sourceOfBusiness ? (this.item.code + '-' + this.resourceDetail.sourceOfBusiness) : null
       console.log("this.sourceOfBusiness", this.sourceOfBusiness);
-
       console.log("RESOURCE", this.resourceDetail)
-
       this.leadDetailService.getStatusById(this.resourceDetail.leadId).toPromise().then(res => {
         if (res) {
           this.statusCode = res;
         }
       })
-
       if (!this.resourceDetail) {
         this.location.back()
         return
       }
       let pageUI: ProductPages = JSON.parse(this.item.config);
-
       if (this.productService.previewType == 'quotation') {
         this.pageOrder = pageUI.quotation || []
         this.detailInput = pageUI.quotation_input || {}
@@ -125,7 +119,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       // }
       let checkTravel = this.pageOrder.findIndex(x => x.id == 'static_1648784270356')
       // console.log(checkTravel);
-
       if (checkTravel >= 0) {
         let checkProd = this.pageOrder.findIndex(x => x.tableName == "travel_detail")
         if (checkProd >= 0) {
@@ -145,14 +138,12 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
         }
       }
       // console.log(this.pageOrder);
-
       // if (this.item.coverages && this.item.coverages.length > 0) {
       //   this.coverage = {
       //     sumInsured: this.item.coverages[0].sumInsured,
       //     unit: this.item.coverages[0].unit,
       //     premium: this.item.coverages[0].premium,
       //   }
-
       //   for (const item of this.item.coverages) {
       //     let response: any = {};
       //     try {
@@ -160,16 +151,13 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       //         response = await this.coverageQuo.getOne(item.id, this.resourceDetail.id).toPromise()
       //       }
       //     } catch (error) {
-
       //     }
       //     this.coverageData[item.id] = {
       //       sum: response ? response.sumInsured || 0 : 0,
       //       unit: response ? response.unit || 0 : 0,
       //       premium: response ? response.premium || 0 : 0
       //     }
-
       //   }
-
       // }
       // if (this.item.addOns && this.item.addOns.length > 0) {
       //   this.addon = {
@@ -184,7 +172,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       //         response = await this.addonQuo.getOne(item.id, this.resourceDetail.id).toPromise()
       //       }
       //     } catch (error) {
-
       //     }
       //     this.addOnData[item.id] = {
       //       sum: response ? response.sumInsured || 0 : 0,
@@ -199,7 +186,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       ]).toPromise().then((res: any) => {
         if (res) {
           console.log("SOC", res[1]);
-
           this.branchOption = res[0]
           this.sourceOfBusinessOption = res[1]
           this.cdf.detectChanges()
@@ -207,10 +193,8 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
             this.selectedBranchCode = this.branch
             let branch = this.branchOption.find((p) => p.code == this.branch)
             // console.log(branch);
-
             this.productService.editData.branch = branch.value
           }
-
           if (this.sourceOfBusiness) {
             this.selectedSourceOfBusiness = this.sourceOfBusiness
             let ss = this.sourceOfBusiness.split('-')
@@ -223,19 +207,21 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       })
     }
   }
-
   ngOnDestroy() {
     this.productService.createingProd = null
     this.productService.editData = null;
   }
-
   getDetail(tempFormData) {
     this.getOldData(this.resourceDetail, tempFormData)
   }
-
+  getQuoResult(event) {
+    if (this.item.code == 'PLMO01' || this.item.code == 'PLMO02') {
+      console.log("EVE", event);
+      this.quoResult = event
+    }
+  }
   checkDisabled() {
     let isDisabled = true
-
     if (this.resourceDetail.apiStatus != null) {
       let status = this.resourceDetail.apiStatus.toLowerCase()
       if (status != 'fail') {
@@ -254,7 +240,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
     }
     return isDisabled
   }
-
   getOldData(oldData: any, tempFormData) {
     let index = 0
     // this.pageOrder.forEach((element) => {
@@ -266,8 +251,7 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
         this.pageOrder[index].tableName = page.tableName
         this.pageOrder[index].id = page.id
         this.pageOrder[index].controls = page.controls
-        console.log("this.pageOrder[index].controls ", this.pageOrder[index].controls);
-
+        // console.log("this.pageOrder[index].controls ", this.pageOrder[index].controls);
         this.pageDataService.getDetail(page.tableName, oldData.id, page.id, true, page.controls).toPromise().then((res: any[]) => {
           if (res) {
             let temp = page.pageType == 'form' ? {} : []
@@ -288,7 +272,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
             };
             this.resultObj[page.tableName + page.id] = temp
             this.formatedData = JSON.parse(JSON.stringify(this.resultObj))
-
             if (lengthData == index) {
               this.cdf.detectChanges()
             }
@@ -296,19 +279,15 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
         })
       }
       index += 1
-
     }
     this.cdf.detectChanges()
     // });
-
   }
-
   download(cols: string[], data: any) {
     let value = this.getOtherDataID(cols, data)
     let fileName = this.getOtherData(cols, data)
     if (value) {
       let valueId = value.split("].")[0].replace("[", "")
-      
       this.downloadService.getDownload(valueId, fileName)
     }
   }
@@ -318,15 +297,14 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
     this.downloadService.getDownload(file[1], test[1])
   }
   getFileName(data: any) {
-    if(data){
-      if(data.includes('[')){
+    if (data) {
+      if (data.includes('[')) {
         let test = data.split("].")
         return test[1]
       }
     }
     return data
   }
-
   getFormatTable(controls: ConfigInput[]) {
     // let tableReform = []
     // let parentArray: string[] = []
@@ -359,7 +337,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
     //   }
     // }
     // return tableReform
-
     let tableReform = []
     let parentArray: string[] = []
     let tempControls: ConfigInput[] = JSON.parse(JSON.stringify(controls))
@@ -411,10 +388,8 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       }
     }
     // console.log("TABLE",tableReform);
-
     return tableReform
   }
-
   getOtherData(cols: any[], data: any) {
     for (let col of cols) {
       if (data[col.name]) {
@@ -440,9 +415,7 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       }
     }
   }
-
   getOtherDataID(cols: any[], data: any) {
-
     for (let col of cols) {
       if (data[col.name]) {
         if ((data[col.name] + "").length > 0) {
@@ -469,9 +442,7 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       }
     }
   }
-
   findPageValue(array: ConfigPage[], value: string) {
-
     let data: FromGroupData
     for (let index = 0; index < array.length; index++) {
       if (!array[index]) continue
@@ -486,15 +457,12 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
           }
           return x
         })
-
         break
       }
       // break
     }
-
     return data
   }
-
   rechangePageData(tempFormData) {
     for (let index = 0; index < this.pageOrder.length; index++) {
       const element = this.pageOrder[index];
@@ -503,12 +471,10 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
         this.pageOrder[index].tableName = page.tableName
         this.pageOrder[index].id = page.id
         this.pageOrder[index].controls = page.controls
-
       }
     }
     // this.pageOrder.forEach((element) => {
     //   if (element.id != 'coverage' && element.id != 'addon') {
-
     //   }
     // })
   }
@@ -557,7 +523,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       this.selectedSourceOfBusiness = null
     }
   }
-
   goToList() {
     // if (this.type == 'quotation') {
     //   this.productService.createingProd = null;
@@ -567,7 +532,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
     //   this.router.navigateByUrl("/sales/application/list")
     this.location.back()
   }
-
   createPolicy() {
     this.productService.createingProdRef = this.item
     this.productService.viewType = 'policy'
@@ -583,7 +547,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       return checkVaidDep(dependency, { value: checkData })
     return true
   }
-
   getOptionValue(value, options: OptionValue[], id: string, name: string) {
     let valueData = options.find(x => x.value == value)
     if (valueData) {
@@ -593,7 +556,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
     }
     return value
   }
-
   viewPrint() {
     if (!this.selectedBranchCode && this.type == 'policy') {
       this.alertService.activate("Please select Branch and Save first.", 'Warning Message')
@@ -643,9 +605,16 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
   //   tableName:"attachment"
   //   type:"policy"
   // }
-
+  Test() {
+    if (!this.quoResult) {
+      this.alertService.activate("Somethings was wrong in Coverage Data", 'Warning Message')
+    }
+  }
+  
   async submitPolicyWithProposal() {
-
+    if (!this.quoResult) {
+      this.alertService.activate("Somethings was wrong in Coverage Data", 'Warning Message')
+    }
     if (!this.selectedBranchCode) {
       this.alertService.activate("Please select Branch and Save first.", 'Warning Message')
     }
@@ -660,7 +629,7 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
   }
   getEmailInfo() {
     this.policyService.getEmailInfo(this.branch, this.item.code).toPromise().then((res) => {
-      console.log(res);
+      // console.log(res);
       if (res) {
         this.emailInfo = res
         if (this.emailInfo) {
@@ -677,8 +646,8 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
           //FOR_AUTO_ATTACHMENT
           modalRef.componentInstance.isPrint = false
           modalRef.result.then(() => { }, (res) => {
-            console.log("submitPolicyWithProposal", res);
-            if (res) {
+            // console.log("submitPolicyWithProposal", res);
+            if (res.data) {
               this.alertService.activate('This record was submitted', 'Success Message');
               this.resourceDetail.apiStatus = 'sending'
               this.resourceDetail.status = 'submitted'
@@ -691,7 +660,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       }
     })
   }
-
   createSign() {
     const modalRef = this.modalService.open(SignaturePadComponent, { size: 'md', backdrop: false });
     modalRef.result.then(() => { }, (res) => {
@@ -709,7 +677,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       }
     })
   }
-
   submitPolicy() {
     if (!this.selectedBranchCode) {
       this.alertService.activate("Please select Branch and Save first.", 'Warning Message')
@@ -726,7 +693,6 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
       })
     }
   }
-
   getBranch() {
     return this.masterDataService.getDataByType("CORE_BRANCH").pipe(map(x => this.getFormatOpt(x)), catchError(e => {
       return of([])
@@ -739,15 +705,12 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
     return this.masterDataService.getDataByParent("PRODUCT_SOB", this.item.code, 'PRODUCT').pipe(map(x => this.getFormatOpt(x)), catchError(e => {
       return of([])
     }))
-
   }
-
   getFormatOpt(res) {
     return res.map(x => {
       return { 'code': x.codeId, 'value': x.codeName || x.codeValue }
     })
   }
-
   changeBranch(event: any) {
     // if (event) {
     //   this.selectedBranchCode = event.code
@@ -755,6 +718,5 @@ export class ResourseDetailComponent implements OnInit, OnDestroy {
     // } else {
     //   this.selectedBranchCode = null
     // }
-
   }
 }
--- src/app/pages/static-page-review/coverage-view/coverage-view.component.ts
+++ src/app/pages/static-page-review/coverage-view/coverage-view.component.ts
@@ -1,9 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { GlobalFunctionService } from '../../../core/global-fun.service';
 import { Product } from '../../products/models/product.dto';
 import { CoverageQuoService } from '../../products/services/coverage-quo.service';
 const CoverageViewID = "coverage_1634010995936"
-
 @Component({
   selector: 'app-coverage-view',
   templateUrl: './coverage-view.component.html',
@@ -12,7 +11,7 @@ const CoverageViewID = "coverage_1634010995936"
 export class CoverageViewComponent implements OnInit {
   @Input() product: Product
   @Input() resourcesId?: string
-  @Input()  optionId?: string
+  @Input() optionId?: string
   coverage = {
     sumInsured: false,
     unit: false,
@@ -23,11 +22,9 @@ export class CoverageViewComponent implements OnInit {
     private coverageQuo: CoverageQuoService,
     private globalFun: GlobalFunctionService,
   ) { }
-
   ngOnInit() {
     this.optionId = this.optionId ? this.optionId : this.resourcesId
     if (this.product.coverages && this.product.coverages.length > 0) {
-      
       this.coverage = {
         sumInsured: this.product.coverages[0].sumInsured,
         unit: this.product.coverages[0].unit,
@@ -36,22 +33,25 @@ export class CoverageViewComponent implements OnInit {
       for (const item of this.product.coverages) {
         let response: any = {};
         if (this.resourcesId) {
-          this.coverageQuo.getOne(item.id, this.resourcesId,this.optionId).toPromise().then((response: any) => {
+          this.coverageQuo.getOne(item.id, this.resourcesId, this.optionId).toPromise().then((response: any) => {
             if (response) {
+              this.globalFun.quotationRes(true)
               
               this.coverageData[item.id] = {
                 sum: response ? Number(response.sumInsured.replace(',', '')) || 0 : 0,
                 unit: response ? response.unit || 0 : 0,
-                premium: response ?  Number(response.premium.replace(',', '')) || 0 : 0
+                premium: response ? Number(response.premium.replace(',', '')) || 0 : 0
               }
+            } else {
+              console.log("FALSE");
+              this.globalFun.quotationRes(false)
+             
             }
           })
         }
-
       }
     }
   }
-
   async getDetail() {
     if (this.product.coverages && this.product.coverages.length > 0) {
       this.coverage = {
@@ -63,20 +63,16 @@ export class CoverageViewComponent implements OnInit {
         let response: any = {};
         try {
           if (this.resourcesId) {
-            response = await this.coverageQuo.getOne(item.id, this.resourcesId,this.optionId).toPromise().catch(e=>e)
+            response = await this.coverageQuo.getOne(item.id, this.resourcesId, this.optionId).toPromise().catch(e => e)
           }
         } catch (error) {
-
         }
         this.coverageData[item.id] = {
           sum: response ? Number(response.sumInsured.replace(',', '')) || 0 : 0,
           unit: response ? response.unit || 0 : 0,
-          premium: response ?  Number(response.premium.replace(',', '')) || 0 : 0
+          premium: response ? Number(response.premium.replace(',', '')) || 0 : 0
         }
-
       }
-
     }
   }
-
 }
